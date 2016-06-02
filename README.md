7098900ba3b0298917fe986735f298e1e950957e
Andrew Wei <andrew@variante.io>
2016-06-03 01:57:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# gulp-task-videos [![Circle CI](https://circleci.com/gh/VARIANTE/gulp-task-videos/tree/master.svg?style=svg)](https://circleci.com/gh/VARIANTE/gulp-task-videos/tree/master) [![npm version](https://badge.fury.io/js/gulp-task-videos.svg)](https://badge.fury.io/js/gulp-task-videos)
+
+Gulp task for processing videos with the option to watch the emitted source files for changes.
+
+## Usage
+
+```js
+import gulp from 'gulp';
+import videos from 'gulp-task-videos';
+
+gulp.task('videos', videos(options));
+```
+
+```
+$ gulp videos
+```
+
+## API
+
+### `videos(options)`
+
+#### `options`
+
+Type: `Object`<br>
+Default: `{ src: '**/*' }`
+
+Options that define the behavior of this task. You can override options for specific `NODE_ENV` environments by putting the same option inside `options.envs.{NODE_ENV}`. For example:
+
+```js
+{
+  src: '**/*',
+  envs: {
+    production: {
+      src: 'foo/**/*'
+    }
+  }
+}
+```
+
+...would give you the following when `NODE_ENV` is `production`:
+
+```js
+{
+  src: 'foo/**/*'
+}
+```
+
+When `NODE_ENV` is blank, `production` environment is assumed.
+
+
+##### `options.base` (required)
+
+Type: `string`
+
+Base path for the source files to emit into the stream.
+
+##### `options.src`
+
+Type: `string` or `Array`<br>
+Default: `**/*`
+
+Glob or an array of globs that matches files to emit. These globs are all relative to `options.base`.
+
+##### `options.dest` (required)
+
+Type: `string`
+
+Path of destination directory to write files to.
+
+##### `options.watch`
+
+Type: `string`, `Function` or `Array`<br>
+Default: Current task name
+
+Task(s) or methods to invoke whenever watched files have changed. This array is applied to [`run-sequence`](https://www.npmjs.com/package/run-sequence). Defaults to the current task name.
+
+## Watching for Changes
+
+You can pass a `--watch` or `--w` flag to the Gulp command to enable file watching, like so:
+
+```
+$ gulp videos --watch
+```
+
+Files that were emitted as source files will be marked for watching, and by default the task name assigned to this module will be executed whenever a file changes. To override this behavior use `options.watch`.
+
+## License
+
+This software is released under the [MIT License](http://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,63 @@
+// (c) VARIANTE
+/**
+ * @file Gulp task for processing videos files, with the option to watch for
+ *       changes by passing either `--watch` or `--w` flag when running the
+ *       task using the CLI.
+ */
+
+const $ = require('gulp-task-helpers');
+const sequence = require('run-sequence');
+const size = require('gulp-size');
+const util = require('gulp-util');
+
+const FILE_EXTENSIONS = ['mov', 'avi', 'ogg', 'ogv', 'webm', 'flv', 'swf', 'mp4', 'mv4'];
+
+const DEFAULT_CONFIG = {
+  base: undefined,
+  dest: undefined,
+  src: '**/*',
+  watch: undefined
+};
+
+/**
+ * Method that defines the task with configurable options. Only `options.base`
+ * and `options.dest` are required.
+ *
+ * @param {Object} options - Task options.
+ * @param {string} options.base - Base path for the source files to emit.
+ * @param {string} options.dest - Path of destination directory to write files
+ *                                to.
+ * @param {string|Array} [options.src='**\/*'] - Glob or an array of globs that
+ *                                               matches files to emit. These
+ *                                               globs are all relative to
+ *                                               `options.base`.
+ * @param {string|Function|Array} [options.watch] - Task(s) or methods to invoke
+ *                                                  whenever watched files have
+ *                                                  changed. This array is
+ *                                                  applied to `run-sequence`.
+ *                                                  Defaults to the current
+ *                                                  task name.
+ *
+ * @return {Function} - A function that returns a Gulp stream.
+ */
+module.exports = function(options) {
+  const config = $.config(options, DEFAULT_CONFIG);
+  let isWatching = false;
+
+  return function() {
+    const taskName = this.seq[0];
+    const shouldWatch = util.env['watch'] || util.env['w'];
+    const src = $.glob(config.src, { base: config.base, exts: FILE_EXTENSIONS });
+    const dest = $.glob('', { base: config.dest });
+
+    if (shouldWatch && !isWatching) {
+      isWatching = true;
+      this.watch(src, () => { sequence.use(this).apply(null, [].concat(config.watch || [taskName])); });
+    }
+
+    return this
+      .src(src, { base: config.base });
+      .pipe(size({ title: '[videos]', gzip: true }))
+      .pipe(this.dest(dest));
+  }
+};
