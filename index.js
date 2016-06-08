327f08b1a372df8001434fe57379639e648f468d
Andrew Wei <andrew@variante.io>
2016-06-08 14:10:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// (c) VARIANTE
+/**
+ * @file Gulp task for processing miscellaneous files, with the option to watch
+ *       for changes by passing either `--watch` or `--w` flag when running the
+ *       task using the CLI.
+ */
+
+const $ = require('gulp-task-helpers');
+const sequence = require('run-sequence');
+const size = require('gulp-size');
+const util = require('gulp-util');
+
+const FILE_EXTENSIONS = ['*'];
+
+const DEFAULT_CONFIG = {
+  base: undefined,
+  dest: undefined,
+  src: undefined,
+  watch: undefined
+};
+
+/**
+ * Method that defines the task with configurable options. Only `options.src`
+ * and `options.dest` are required.
+ *
+ * @param {Object} options - Task options.
+ * @param {string} [options.base] - Base path for the source files to emit.
+ * @param {string|string[]} options.src - Glob or an array of globs that matches
+ *                                        files to emit. These globs are all
+ *                                        relative to `options.base`.
+ * @param {string} options.dest - Path of destination directory to write files
+ *                                to.
+ * @param {Object} [options.watch=false] - Options that define the file watching
+ *                                         behavior. If set to `false`, watching
+ *                                         will be disabled even if the CLI flag
+ *                                         is set.
+ * @param {string|string[]} [options.watch.files] - Glob pattern(s) that matches
+ *                                                  files to watch. Defaults to
+ *                                                  the emitted files.
+ * @param {string|Function|Array} [options.watch.tasks] - Array of task names or
+ *                                                        functions to execute
+ *                                                        when watched files
+ *                                                        change. Defaults to
+ *                                                        the current task name.
+ * @param {boolean} [extendsDefaults=true] - Specifies whether array values are
+ *                                           concatenated when merging config
+ *                                           options with defaults.
+ *
+ * @return {Function} - A function that returns a Gulp stream.
+ */
+module.exports = function(options, extendsDefaults) {
+  if (typeof extendsDefaults !== 'boolean') extendsDefaults = true;
+  let isWatching = false;
+
+  return function() {
+    const taskName = this.seq[0];
+
+    // Set defaults based on options before merging.
+    if (options.src) {
+      DEFAULT_CONFIG.watch = {
+        files: [$.glob(options.src, { base: options.base, exts: FILE_EXTENSIONS })],
+        tasks: [taskName]
+      }
+    }
+
+    const config = $.config(options, DEFAULT_CONFIG, extendsDefaults);
+    const shouldWatch = (util.env['watch'] || util.env['w']) && (config.watch !== false);
+    const src = $.glob(config.src, { base: config.base, exts: FILE_EXTENSIONS });
+    const dest = $.glob('', { base: config.dest });
+
+    if (shouldWatch && !isWatching) {
+      isWatching = true;
+      this.watch(config.watch.files, () => sequence.use(this).apply(null, [].concat(config.watch.tasks)));
+    }
+
+    return this
+      .src(src, { base: config.base })
+      .pipe(size({ title: `[${taskName}]`, gzip: true }))
+      .pipe(this.dest(dest));
+  }
+};
