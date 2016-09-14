73bfd99ed597b5646b5802f12f77a1350997f719
Andrew Wei <andrew@andr.mu>
2016-09-14 16:42:01 -0700

    Add new helpers

---
+++ helpers/getPath.js
@@ -0,0 +1,31 @@
+// (c) Andrew Wei
+
+/**
+ * Resolves paths in view files. This method ensures that there are no
+ * relative paths (by prefixing paths with '/' if needed) and resolves
+ * fingerprinted paths.
+ *
+ * @param {string} p
+ * @param {string} manifestPath
+ *
+ * @return {string}
+ */
+function getPath(p, manifestPath) {
+  // Ensure there are no relative paths.
+  let regex = /^(?!.*:\/\/)\w+.*$/g
+  if (regex.test(p)) p = `/${p}`;
+
+  try {
+    let manifest = require(manifestPath);
+    if (!manifest) return p;
+    let r = manifest[p] || manifest[p.substr(1)];
+    if (!r) return p;
+    if (regex.test(r)) r = `/${r}`;
+    return r;
+  }
+  catch (err) {
+    return p;
+  }
+}
+
+module.exports = getPath;
