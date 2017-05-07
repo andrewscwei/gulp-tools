5c68e051b5be5a03f2038b712a727ca30f879d5b
Andrew Wei <andrewscwei@gmail.com>
2017-05-06 19:53:40 -0700

    Update example

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const stylus = require('../');
 
 gulp.task('default', stylus({
-  base: './',
+  base: 'app',
   src: 'stylesheets/*',
   dest: 'public',
   sourcemaps: true
