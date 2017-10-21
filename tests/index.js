3c91e9d9fc7315741e726114fcb3c27d6c9b0643
Andrew Wei <andrewscwei@gmail.com>
2017-10-20 19:57:14 -0700

    Update formatting

--- tests/index.js
+++ tests/index.js
@@ -1,33 +1,33 @@
-// (c) Andrew Wei
+// © Andrew Wei
 
 'use strict';
 
-const $ = require('../');
-const expect = require('chai').expect;
+const $ = require(`../`);
+const expect = require(`chai`).expect;
 
-describe('helpers', function() {
-  it('glob() should work with string patterns', function() {
-    expect($.glob('a/b')).to.eql('a/b');
+describe(`helpers`, function() {
+  it(`glob() should work with string patterns`, function() {
+    expect($.glob(`a/b`)).to.eql(`a/b`);
   });
 
-  it('glob() should work with array patterns', function() {
-    expect($.glob(['a/b', 'b/c'])).to.eql(['a/b', 'b/c']);
+  it(`glob() should work with array patterns`, function() {
+    expect($.glob([`a/b`, `b/c`])).to.eql([`a/b`, `b/c`]);
   });
 
-  it('glob() should work with base option', function() {
-    expect($.glob('a/b', { base: 'z' })).to.eql('z/a/b');
-    expect($.glob(['a/b', 'b/c'], { base: 'z' })).to.eql(['z/a/b', 'z/b/c']);
+  it(`glob() should work with base option`, function() {
+    expect($.glob(`a/b`, { base: `z` })).to.eql(`z/a/b`);
+    expect($.glob([`a/b`, `b/c`], { base: `z` })).to.eql([`z/a/b`, `z/b/c`]);
   });
 
-  it('glob() should work with exts option', function() {
-    expect($.glob('a/b', { exts: ['a'] })).to.eql('a/b.a');
-    expect($.glob('a/b', { exts: [] })).to.eql('a/b');
-    expect($.glob('a/b', { exts: ['a', 'b'] })).to.eql('a/b.{a,b}');
-    expect($.glob('a/b', { exts: [['a', 'b'], ['c', 'd']] })).to.eql('a/b.{a,b,c,d}');
-    expect($.glob(['a/b', 'c/d'], { exts: [['a', 'b'], ['c', 'd']] })).to.eql(['a/b.{a,b,c,d}', 'c/d.{a,b,c,d}']);
+  it(`glob() should work with exts option`, function() {
+    expect($.glob(`a/b`, { exts: [`a`] })).to.eql(`a/b.a`);
+    expect($.glob(`a/b`, { exts: [] })).to.eql(`a/b`);
+    expect($.glob(`a/b`, { exts: [`a`, `b`] })).to.eql(`a/b.{a,b}`);
+    expect($.glob(`a/b`, { exts: [[`a`, `b`], [`c`, `d`]] })).to.eql(`a/b.{a,b,c,d}`);
+    expect($.glob([`a/b`, `c/d`], { exts: [[`a`, `b`], [`c`, `d`]] })).to.eql([`a/b.{a,b,c,d}`, `c/d.{a,b,c,d}`]);
   });
 
-  it('config() should work without defaults', function() {
+  it(`config() should work without defaults`, function() {
     expect($.config({
       a: 1,
       b: 2
@@ -37,7 +37,7 @@ describe('helpers', function() {
     });
   });
 
-  it('config() should work with defaults', function() {
+  it(`config() should work with defaults`, function() {
     expect($.config({
       a: 1,
       b: 2
